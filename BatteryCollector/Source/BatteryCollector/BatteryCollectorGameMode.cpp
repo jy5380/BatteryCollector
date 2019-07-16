f710a870edc2e33eac764633cc3536ca0a7b03c9
jiangyuan <jiangyuan5380@gmail.com>
2019-07-16 15:45:20 +0800

    first

---
+++ BatteryCollector/Source/BatteryCollector/BatteryCollectorGameMode.cpp
@@ -0,0 +1,153 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "BatteryCollectorGameMode.h"
+#include "BatteryCollectorCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+#include "GameFramework/Character.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Blueprint/UserWidget.h"
+#include "SpawnVolume.h"
+
+ABatteryCollectorGameMode::ABatteryCollectorGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+
+	PrimaryActorTick.bCanEverTick = true;
+	DecayRate = 0.01f;
+}
+
+void ABatteryCollectorGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	ABatteryCollectorCharacter* MyCharacter = Cast<ABatteryCollectorCharacter>(UGameplayStatics::GetPlayerPawn(this, 0));
+	if (MyCharacter)
+	{
+		if (MyCharacter->GetCurrentPower() > PowerToWin) 
+		{
+			SetCurrentState(EBatteryPlayState::EWon);
+		}
+		else if (MyCharacter->GetCurrentPower() > 0)
+		{
+			MyCharacter->UpdatePower(-DeltaTime * DecayRate * (MyCharacter->GetInitialPower()));
+		}
+		else
+		{
+			SetCurrentState(EBatteryPlayState::EGameOver);
+		}
+	}
+}
+
+float ABatteryCollectorGameMode::GetPowerToWin() const
+{
+	return PowerToWin;
+}
+
+void ABatteryCollectorGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TArray<AActor*> FoundActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ASpawnVolume::StaticClass(), FoundActors);
+
+	for (auto Actor : FoundActors)
+	{
+		ASpawnVolume* SpawnVolumeActor = Cast<ASpawnVolume>(Actor);
+		if (SpawnVolumeActor)
+		{
+			SpawnVolumeActors.AddUnique(SpawnVolumeActor);
+		}
+	}
+
+	SetCurrentState(EBatteryPlayState::EPlaying);
+
+	ABatteryCollectorCharacter* MyCharacter = Cast<ABatteryCollectorCharacter>(UGameplayStatics::GetPlayerPawn(this, 0));
+	if (MyCharacter)
+	{
+		PowerToWin = (MyCharacter->GetInitialPower()) * 1.25f;
+	}
+
+	if (HUDWidgetClass != nullptr)
+	{
+		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), HUDWidgetClass);
+		if (CurrentWidget != nullptr)
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}	
+}
+
+EBatteryPlayState ABatteryCollectorGameMode::GetCurrentState() const
+{
+	return CurrentState;
+}
+
+void ABatteryCollectorGameMode::SetCurrentState(EBatteryPlayState NewState)
+{
+	CurrentState = NewState;
+	HandleNewState(CurrentState);
+}
+
+void ABatteryCollectorGameMode::HandleNewState(EBatteryPlayState NewState)
+{
+	switch (NewState)
+	{
+	case EBatteryPlayState::EPlaying:
+	{
+		for (ASpawnVolume* Volume : SpawnVolumeActors)
+		{
+			Volume->SetSpawningActive(true);
+		}
+	}
+	break;
+
+	case EBatteryPlayState::EWon:
+	{
+		for (ASpawnVolume* Volume : SpawnVolumeActors)
+		{
+			Volume->SetSpawningActive(false);
+		}
+	}
+	break;
+
+	case EBatteryPlayState::EGameOver:
+	{
+		for (ASpawnVolume* Volume : SpawnVolumeActors)
+		{
+			Volume->SetSpawningActive(false);
+		}
+
+		APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0);
+		if (PlayerController)
+		{
+			PlayerController->SetCinematicMode(true, false, false, true, true);
+		}
+
+		ACharacter* MyCharacter = UGameplayStatics::GetPlayerCharacter(this, 0);
+		if (MyCharacter)
+		{
+			MyCharacter->GetMesh()->SetSimulatePhysics(true);
+			MyCharacter->GetMovementComponent()->MovementState.bCanJump = false;
+		}
+	}
+	break;
+
+	default:
+	case EBatteryPlayState::EUnknown:
+	{
+
+	}
+	break;
+	}
+}
+
+
+
+
---
+++ BatteryCollector/Source/BatteryCollector/BatteryPickup.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup.h"
+#include "BatteryPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTERYCOLLECTOR_API ABatteryPickup : public APickup
+{
+	GENERATED_BODY()
+
+public:
+
+	ABatteryPickup();
+
+	void WasCollected_Implementation() override;
+
+
+	float GetPower();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Power", Meta = (BlueprintProtected = "true"))
+	float BatteryPower;
+	
+};
